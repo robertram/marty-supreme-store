a69535878db01befc5893634fd9482fee4156b02
Robert <37461123+robertram@users.noreply.github.com>
2025-11-28 13:15:44 -0600

    feat-2: main hero schema and content display

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports ={
-  purge: [], 
+  content: [
+    './layout/**/*.liquid',
+    './sections/**/*.liquid',
+    './snippets/**/*.liquid',
+    './templates/**/*.liquid',
+    './templates/**/*.json'
+  ],
   darkMode: false, 
   theme: {
     extend: {
+      colors: {
+        primary: '#FD7603'
+      },
       height: {
         '94': '22rem', 
       }
